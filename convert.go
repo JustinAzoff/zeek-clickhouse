b56e7f6ca0cd1bb58627bf4fc4080627619c188d
Justin Azoff <justin@azoff.dev>
2021-03-07 15:42:27 -0500

    refactor main

--- convert.go
+++ convert.go
@@ -1,4 +1,4 @@
-package main
+package zeekclickhouse
 
 import (
 	"fmt"
--- inserter.go
+++ inserter.go
@@ -1,13 +1,9 @@
-package main
+package zeekclickhouse
 
 import (
-	"bufio"
 	"database/sql"
 	_ "embed"
-	"flag"
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/ClickHouse/clickhouse-go"
 )
@@ -82,57 +78,4 @@ func (i *Inserter) Insert(rec DBRecord) error {
 		clickhouse.Array(rec.array_names), clickhouse.Array(rec.array_values),
 	)
 	return err
-}
-
-func main() {
-	var uri string
-	flag.StringVar(&uri, "uri", "tcp://192.168.2.68:9000?debug=false", "server uri")
-	flag.Parse()
-
-	inserter, err := NewInserter(uri)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := inserter.Ping(); err != nil {
-		if exception, ok := err.(*clickhouse.Exception); ok {
-			fmt.Printf("[%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
-		} else {
-			fmt.Println(err)
-		}
-		return
-	}
-	err = inserter.LoadSchema()
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = inserter.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-	br := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
-	for {
-		line, err := br.ReadSlice('\n')
-		if err != nil {
-			log.Printf("ReadSlice: %v", err)
-			break
-		}
-		rec, err := ZeekToDBRecord(line)
-		if err != nil {
-			log.Printf("Error Converting: %s: %v", line, err)
-			continue
-		}
-		//log.Printf("Strings: %v %v", string_names, string_values)
-		//log.Printf("numbers: %v %v", number_names, number_values)
-		//log.Printf("bools: %v %v", bool_names, bool_values)
-		//log.Printf("arrays: %v %v", array_names, array_values)
-		err = inserter.Insert(rec)
-		if err != nil {
-			log.Printf("Error inserting: %v: %v", rec, err)
-			continue
-		}
-	}
-
-	if err := inserter.Commit(); err != nil {
-		log.Fatal(err)
-	}
 }
