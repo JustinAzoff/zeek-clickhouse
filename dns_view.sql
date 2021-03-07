8dbc583e5894ca0b094343182bc94e1d7ce169e7
Justin Azoff <justin@azoff.dev>
2021-03-07 09:35:42 -0500

    add view for dns log

---
+++ dns_view.sql
@@ -0,0 +1,27 @@
+create view dns as
+select
+	_timestamp,
+	ts,
+	day,
+	_hostname,
+	_source,
+
+	uid,
+	"id.orig_h", "id.orig_p",
+	"id.resp_h", "id.resp_p",
+	`string.values`[indexOf(`string.names`, 'proto')] AS proto,
+	`number.values`[indexOf(`number.names`, 'trans_id')] AS trans_id,
+	`string.values`[indexOf(`string.names`, 'query')] AS query,
+	`number.values`[indexOf(`number.names`, 'rcode')] AS rcode,
+	`string.values`[indexOf(`string.names`, 'rcode_name')] AS rcode_name,
+	`bool.values`[indexOf(`bool.names`, 'AA')] AS AA,
+	`bool.values`[indexOf(`bool.names`, 'TC')] AS TC,
+	`bool.values`[indexOf(`bool.names`, 'RD')] AS RD,
+	`bool.values`[indexOf(`bool.names`, 'RA')] AS RA,
+	`number.values`[indexOf(`number.names`, 'Z')] AS Z,
+	`array.values`[indexOf(`array.names`, 'answers')] AS answers,
+	`array.values`[indexOf(`array.names`, 'TTLs')] AS TTLs,
+	`bool.values`[indexOf(`bool.names`, 'rejected')] AS rejected
+FROM logs
+WHERE _path='dns'
+;
